afc6b4f188d0df08c0d3ccac4f14bbe36ad805cb
eight <eight04@gmail.com>
2018-04-17 20:13:43 +0800

    Failed to request permissions inside the handler of the menu item

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
-browser.tabs.executeScript({
-  code: "'success'",
-  allFrames: true,
-  frameId: 0
-})
-  .then(console.log, console.error);
+browser.menus.create({
+  id: "test",
+  title: "Test",
+  contexts: ["browser_action", "page"],
+  onclick() {
+    browser.permissions.request({permissions: ["webNavigation"]});
+  }
+});
