980b9f805707944c70e545c543b93a82da6201f1
eight <eight04@gmail.com>
2018-04-17 20:21:02 +0800

    Try to request inside onClicked event

--- background.js
+++ background.js
@@ -1,8 +1,24 @@
+function doRequest() {
+  browser.permissions.request({permissions: ["webNavigation"]});
+}
+
 browser.menus.create({
   id: "test",
   title: "Test",
   contexts: ["browser_action", "page"],
   onclick() {
-    browser.permissions.request({permissions: ["webNavigation"]});
+    doRequest();
+  }
+});
+
+browser.menus.create({
+  id: "test2",
+  title: "Test 2",
+  contexts: ["browser_action", "page"]
+});
+
+browser.menus.onClicked.addListener(({menuItemId}) => {
+  if (menuItemId === "test2") {
+    doRequest();
   }
 });
