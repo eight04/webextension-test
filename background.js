9d55c655527396dbfe34e1ed8d859a70f51ed595
eight <eight04@gmail.com>
2019-12-26 01:28:40 +0800

    Chrome displays an empty menu if visible=false

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.contextMenus.create({
+  title: "foo",
+  contexts: ["page"],
+  visible: false
+});
+chrome.contextMenus.create({
+  title: "bar",
+  contexts: ["page"],
+  visible: false
+});
