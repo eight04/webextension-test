1063498264b7481ad553e5cf693dd7b1ac3e7d45
eight <eight04@gmail.com>
2020-02-06 21:12:35 +0800

    launchWebAuthFlow doesn't work with dropbox

--- background.js
+++ background.js
@@ -1,10 +1,17 @@
-chrome.contextMenus.create({
-  title: "foo",
-  contexts: ["page"],
-  visible: false
-});
-chrome.contextMenus.create({
-  title: "bar",
-  contexts: ["page"],
-  visible: false
+chrome.browserAction.onClicked.addListener(() => {
+  const query = {
+    response_type: "token",
+    client_id: "z4ra9yfhsj5e4zg",
+    state: "123",
+    redirect_uri: chrome.identity.getRedirectURL()
+  };
+  const params = new URLSearchParams;
+  for (const key in query) {
+    params.set(key, query[key]);
+  }
+  const url = `https://www.dropbox.com/oauth2/authorize?${params}`;
+  console.log("query: %o, url: %o", query, url);
+  chrome.identity.launchWebAuthFlow({url, interactive: true}, finalUrl => {
+    console.log("result: %o, error: %o", finalUrl, chrome.runtime.lastError);
+  });
 });
