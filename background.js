f5dbdf8c6592d21682888fa9575fa520a50fb6c6
eight04 <eight04@gmail.com>
2021-01-15 15:19:26 +0800

    vivaldi doesn't report onbeforerequest for popup

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
-browser.browserAction.onClicked.addListener(() => {
-  browser.windows.create({type: "popup", state: "minimized"}).catch(console.error);
+browser.webRequest.onBeforeRequest.addListener(console.log, {
+  urls: [
+    "<all_urls>"
+  ]
+});
+
+browser.tabs.create({
+  url: "http://example.com/foo"
+});
+
+browser.windows.create({
+  url: "http://example.com/bar",
+  type: "popup"
 });
