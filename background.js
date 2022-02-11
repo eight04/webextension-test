9a1da97906906ee42c6c59564296c217bf10d47f
eight04 <eight04@gmail.com>
2022-02-11 17:49:00 +0800

    Chrome triggers download even content-type is modified

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
-browser.browserAction.onClicked.addListener(() => {
-  browser.tabs.create({
-    url: browser.runtime.getURL('index.html')
-  })
-    .catch(console.error);
-});
+function listener(details) {
+  let dirty = false;
+  for (const item of details.responseHeaders) {
+    if (item.name.toLowerCase() === "content-type") {
+      item.value = "text/plain";
+      dirty = true;
+    }
+  }
+  if (dirty) {
+    return {responseHeaders: details.responseHeaders};
+  }
+}
+const filter = {
+  urls: ["<all_urls>"]
+};
+const extra = ["blocking", "responseHeaders"];
+browser.webRequest.onHeadersReceived.addListener(listener, filter, extra);
--- index.js
+++
@@ -1,9 +0,0 @@
-fetch(browser.runtime.getURL("test.png"))
-  .then(r => r.blob())
-  .then(b => {
-    const img = new Image;
-    img.src = URL.createObjectURL(b);
-    document.body.append(img);
-  })
-  .catch(console.error);
-  
