43f73b7d11c727791448d3fde2b72ab91f04e03b
eight04 <eight04@gmail.com>
2022-02-17 03:13:40 +0800

    tabs.create in Chrome always place the tab at last

--- background.js
+++ background.js
@@ -1,17 +1,7 @@
-function listener(details) {
-  let dirty = false;
-  for (const item of details.responseHeaders) {
-    if (item.name.toLowerCase() === "content-type") {
-      item.value = "text/plain";
-      dirty = true;
-    }
-  }
-  if (dirty) {
-    return {responseHeaders: details.responseHeaders};
-  }
-}
-const filter = {
-  urls: ["<all_urls>"]
-};
-const extra = ["blocking", "responseHeaders"];
-browser.webRequest.onHeadersReceived.addListener(listener, filter, extra);
+browser.browserAction.onClicked.addListener(tab => {
+  browser.tabs.create({
+    url: "http://example.com",
+    openerTabId: tab.id
+  }).catch(console.error);
+});
+
